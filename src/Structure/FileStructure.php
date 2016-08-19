0761f039f0e66fb60f31f5d39515e347fb140d24
Marcos Felipe @omarkdev <mrkppl@live.com>
2016-08-18 23:51:44 -0300

    FileStructure to get Structure Files

---
+++ src/Structure/FileStructure.php
@@ -0,0 +1,47 @@
+<?php
+namespace BashIgniter\Structure;
+
+class FileStructure{
+
+	private $structureFile;
+
+	public function controller(){
+		return $this->getFile("Controller");
+	}
+
+	public function model(){
+		return $this->getFile("Model");
+	}
+
+	public function view(){
+		return $this->viewEmpty();
+	}
+
+	public function viewEmpty(){
+		return $this->getFile("View");
+	}
+
+	public function viewHtml(){
+		return $this->getFile("ViewHtml");
+	}
+
+	public function helper(){
+		return $this->getFile("Helper");
+	}
+
+	public function core(){
+		return $this->getFile("Core");
+	}
+
+	public function coreExtend(){
+		return $this->getFile("CoreExtend");
+	}
+
+	private function getFile($file){
+		if(is_null($this->structureFile))
+			$this->structureFile = file_get_contents(__DIR__."/Files/{$file}.php");
+
+		return $this->structureFile;
+	}
+}
+?>
